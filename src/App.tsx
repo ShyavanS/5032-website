82f509e8fc4567b5a124ce35dda77a8909c22a15
Farouq Syed Abdali <farouqsyedabdali@gmail.com>
2020-11-21 16:56:53 -0500

    Created a second page, looks very bad but in early stages. Go to /testpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './Falcons.svg';
 import './App.css';
+import testpage from './testpage';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/testpage" component={testpage} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/testpage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function testpage(){
+  return(
+    <div className="test">
+      <h1>Welcome to 5032</h1>
+    </div>
+  );
+}
+
+export default testpage;
