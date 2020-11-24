2411ef1a2aba7773df693552464f3ab1f398c838
Farouq Syed Abdali <farouqsyedabdali@gmail.com>
2020-11-24 16:30:26 -0500

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./styles/App.css";
 import Sponsors from "./pages/sponsors";
 import Home from "./pages/home";
-import NavBar from "./pages/navbar";
-import AboutOurTeam from "./pages/about-our-team";
+import Navbar from "./pages/navbar";
 import History from "./pages/history";
+import About from "./pages/about-our-team";
 import WhoWeAre from "./pages/who-we-are";
 import Footer from "./pages/footer";
 import Documents from "./pages/documents";
@@ -13,12 +13,12 @@ import { Route } from "react-router-dom";
 function App() {
   return (
     <div className='App'>
-      <NavBar />
+      <Navbar />
       <Route exact path='/' component={Home} />
       <Route exact path='/sponsors' component={Sponsors} />
-      <Route exact path='/about-our-team' component={AboutOurTeam} />
+      <Route exact path='/about-our-team' component={About} />
       <Route exact path='/who-we-are' component={WhoWeAre} />
-      <Route exact path='history' component={History} />
+      <Route exact path='/history' component={History} />
       <Route exact path='/documents' component={Documents} />
       <Footer />
     </div>
