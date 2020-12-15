2140e314dfcf050158a19981de3772215c7e137e
ShyavanS <ShyavanS@github.com>
2020-12-15 16:50:29 -0500

    Fixed pictures and added programming lead picture.

--- src/pages/who-we-are.tsx
+++ src/pages/who-we-are.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import mech from "../assets/IMG_20200302_004453_540.jpg";
+import cap from "../assets/IMG_20191126_130032_153.jpg";
+import bus from "../assets/IMG_20201211_170731006.jpg";
+import prog from "../assets/IMG-1447.jpg";
 
-function WhoWeAre() {
+function WhoWeAre(): JSX.Element {
   return (
     <div className='WhoWeAre'>
       <h1>Team Captain</h1>
-      <img src="../assets/IMG_20191126_130032_153.jpg" alt="Team Captain" />
+      <img src={cap} width="10%" alt="Team Captain" />
       <h1>Mechanical Lead</h1>
-      <img src="../assets/IMG_20200302_004453_540.jpg" alt="Mechanical Lead" />
+      <img src={mech} width="10%" alt="Mechanical Lead" />
+      <h1>Programming Lead</h1>
+      <img src={prog} width="10%" alt="Programming Lead" />
       <h1>Business Lead</h1>
-      <img src="../assets/IMG_20201211_170731006.jpg" alt="Business Lead" />
+      <img src={bus} width="10%" alt="Business Lead" />
+      <p></p>
+      <br></br>
+      <p></p>
     </div>
   );
 }
