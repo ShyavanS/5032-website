e6ac825e3e831f01e2e9c6c34333fd49065d7c76
ShyavanS <ShyavanS@github.com>
2020-12-24 23:35:45 -0500

    Added countdowns for website's release and FRC kickoff on main page. (You'll need to run npm install again as a module for the countdowns was installed)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import logo from "../assets/logo.svg";
-import "../components/navbar.css";
 //import team from "../assets/teamphoto.jpg"
+import "../components/navbar.css";
+import '@nilevia/count-down-timer-react/dist/index.css';
+const { CountDownDaily } = require('@nilevia/count-down-timer-react');
 
 function Home() {
   return (
@@ -9,6 +11,28 @@ function Home() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Coming Soon!</p>
+        <p className="Countdowns">
+          FRC Kickoff in...
+          <CountDownDaily
+            endDate={"2021-01-04T12:00:00.000-05:00"}
+            dayAffix="day(s)"
+            hourAffix="hr(s)"
+            minutesAffix="min(s)"
+            secondAffix="sec(s)"
+            className="light-blue bold font-muli-20"
+          />
+          Website's first release in...
+          <CountDownDaily
+            endDate={"2021-01-29T00:00:00.000-05:00"}
+            dayAffix="day(s)"
+            hourAffix="hr(s)"
+            minutesAffix="min(s)"
+            secondAffix="sec(s)"
+            className="light-blue bold font-muli-20"
+          />
+        </p>
+        <p></p>
+        <br></br>
       </header>
       {/*<h1>Team 5032</h1>
       <img src={team} width="50%" alt="The Team"></img>
