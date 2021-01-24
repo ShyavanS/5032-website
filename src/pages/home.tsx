f87b56fa7a11889e3908f4ba857dfe40db1919fb
complicatedstuff <38135845+complicatedstuff@users.noreply.github.com>
2021-01-24 16:18:07 -0500

    added a home.css file

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import "../styles/home.css";
 //import team from "../assets/teamphoto.jpg"
 import "../components/navbar.css";
-import '@nilevia/count-down-timer-react/dist/index.css';
-const { CountDownDaily } = require('@nilevia/count-down-timer-react');
+import "@nilevia/count-down-timer-react/dist/index.css";
+const { CountDownDaily } = require("@nilevia/count-down-timer-react");
 
 function Home() {
   return (
@@ -11,24 +12,24 @@ function Home() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Coming Soon!</p>
-        <p className="Countdowns">
+        <p className='Countdowns'>
           FRC Kickoff in...
           <CountDownDaily
             endDate={"2021-01-04T12:00:00.000-05:00"}
-            dayAffix="day(s)"
-            hourAffix="hr(s)"
-            minutesAffix="min(s)"
-            secondAffix="sec(s)"
-            className="light-blue bold font-muli-20"
+            dayAffix='day(s)'
+            hourAffix='hr(s)'
+            minutesAffix='min(s)'
+            secondAffix='sec(s)'
+            className='light-blue bold font-muli-20'
           />
           Website's first release in...
           <CountDownDaily
             endDate={"2021-01-29T00:00:00.000-05:00"}
-            dayAffix="day(s)"
-            hourAffix="hr(s)"
-            minutesAffix="min(s)"
-            secondAffix="sec(s)"
-            className="light-blue bold font-muli-20"
+            dayAffix='day(s)'
+            hourAffix='hr(s)'
+            minutesAffix='min(s)'
+            secondAffix='sec(s)'
+            className='light-blue bold font-muli-20'
           />
         </p>
         <p></p>
