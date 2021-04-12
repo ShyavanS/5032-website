e43deb9429daed6cd067be76ca86d322a70206ca
Farouq Syed Abdali <farouqsyedabdali@gmail.com>
2021-04-12 13:50:57 -0400

    who we are needs a lot of fixing on the formatting sie

--- src/pages/who-we-are.tsx
+++ src/pages/who-we-are.tsx
@@ -3,18 +3,19 @@ import mech from "../assets/mechlead.jpg";
 import cap from "../assets/teamcap.jpg";
 import bus from "../assets/buslead.jpg";
 import prog from "../assets/proglead.jpg";
+import "../styles/who-we-are.css";
 
 function WhoWeAre(): JSX.Element {
   return (
     <div className='WhoWeAre'>
-      <h1>Team Captain</h1>
-      <img src={cap} width="20%" alt="Team Captain" />
-      <h1>Mechanical Lead</h1>
-      <img src={mech} width="20%" alt="Mechanical Lead" />
-      <h1>Programming Lead</h1>
-      <img src={prog} width="20%" alt="Programming Lead" />
-      <h1>Business Lead</h1>
-      <img src={bus} width="20%" alt="Business Lead" />
+      <h1 className='textleft'>Team Captain</h1>
+      <img className='imgleft' src={cap} alt="Team Captain" />
+      <h1 className='textright'>Mechanical Lead</h1>
+      <img className='imgright' src={mech} alt="Mechanical Lead" />
+      <h1 className='textleft'>Programming Lead</h1>
+      <img className='imgleft' src={prog} alt="Programming Lead" />
+      <h1 className='textright'>Business Lead</h1>
+      <img className='imgright' src={bus} width="20%" alt="Business Lead" />
       <p></p>
       <br></br>
       <p></p>
