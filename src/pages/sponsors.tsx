6e6bdf4582c7af6a8cff96e78270466c39d85764
ShyavanS <shyavanthecoolguy@gmail.com>
2021-05-02 16:25:55 -0400

    updated images

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import thk from "../assets/thklogo.png";
 import conovey from "../assets/conoveylogo.png";
-import sikhmc from "../assets/smc_logo.png";
-import gm from "../assets/general-motors-logo.png";
+import smc from "../assets/smclogo.png";
+import gm from "../assets/gmlogo.png";
 import matman from "../assets/matmanlogo.png";
 import starmill from "../assets/starmilllogo.png";
 import "../styles/sponsors.css";
@@ -10,14 +10,12 @@ import "../styles/sponsors.css";
 function Sponsors() {
   return (
     <div className='Sponsors'>
-      <img src={thk} width="30%" />
-      <br></br>
-      <img className="conoveylogo" src={conovey} width="40%" />
-      <img className="sikhlogo" src={sikhmc} width="10%" />
-      <img src={gm} width="20%" />
-      <br></br>
-      <img className="matman" src={matman} width="30%" />
-      <img className="starmill" src={starmill} width="30%" />
+      <img src={thk} alt="THK Logo" />
+      <img src={conovey} alt="Conovey Logo" />
+      <img src={smc} alt="SMC Logo" />
+      <img src={gm} alt="GM Logo" />
+      <img src={matman} alt="MatMan Logo" />
+      <img src={starmill} alt="Starmill Manufacturing Logo" />
     </div>
   );
 }
