32f7a5c0e08ca24c835de37709212c86a796a56e
Farouq Syed Abdali <farouqsyedabdali@gmail.com>
2021-05-02 15:54:50 -0400

    added sponsors logos

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import thk from "../assets/thklogo.png";
+import conovey from "../assets/conoveylogo.png";
+import sikhmc from "../assets/smc_logo.png";
+import gm from "../assets/general-motors-logo.png";
+import matman from "../assets/matmanlogo.png";
+import starmill from "../assets/starmilllogo.png";
+import "../styles/sponsors.css";
 
 function Sponsors() {
   return (
     <div className='Sponsors'>
-      <h1>No sponsors yet</h1>
+      <img src={thk} width="30%" />
+      <br></br>
+      <img className="conoveylogo" src={conovey} width="40%" />
+      <img className="sikhlogo" src={sikhmc} width="10%" />
+      <img src={gm} width="20%" />
+      <br></br>
+      <img className="matman" src={matman} width="30%" />
+      <img className="starmill" src={starmill} width="30%" />
     </div>
   );
 }
